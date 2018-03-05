1d56536046a80ae1e72c9483878b6a8b19581c27
Lauri Himanen <lauri.himanen@aalto.fi>
2018-03-05 19:36:23 +0200

    Added a repository URL, removed long description from setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,20 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(name="systax",
-        version="0.1.2",
+        version="0.2.0",
+        url="https://gitlab.com/laurih/systax",
         description=(
-            "Systax is a python package for the analysis and classification of "
-            "atomistic systems."
-        ),
-        long_description=(
             "Systax is a python package for the analysis and classification of "
             "atomistic systems."
         ),
         author='Lauri Himanen',
         author_email='lauri.himanen@aalto.fi',
-        license="LGPL3",
+        license="Apache 2.0",
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
             'Topic :: Scientific/Engineering :: Physics',
-            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'License :: OSI Approved :: Apache Software License',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
